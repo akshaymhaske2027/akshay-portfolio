9cd18f255b298cf378c9e4c22451e9b4f7212674
AKSHAY MHASKE <akshaymhaske2027@gmail.com>
2024-07-07 21:43:06 +0530

    FInished

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,72 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img-2.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import instagramLight from "../../assets/insta-light.svg";
+import instagramDark from "../../assets/insta-dark.svg";
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const instagramIcon = theme === "light" ? instagramLight : instagramDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Akshay Mhaske"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Akshay
+          <br />
+          Mhaske
+        </h1>
+        <h2>Full Stack Developer(JAVA)</h2>
+        <span>
+        
+          <a href="https://twitter.com/x_balyaa" target="_blank">
+            <img src={twitterIcon} alt="Twitter Icon" />
+          </a>
+          <a href="https://github.com/akshaymhaske2027/" target="_blank">
+            <img src={githubIcon} alt="Github Icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/akshay-mhaske/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin Icon" />
+          </a>
+          <a href="https://instagram.com/akshay_mhaske___/" target="_blank">
+            <img src={instagramIcon} alt="Instagram Icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+        Passionate about crafting advanced Java-based web applications.</p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,40 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
+
+function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="J2EE" />
+        <SkillList src={checkMarkIcon} skill="Spring" />
+        <SkillList src={checkMarkIcon} skill="Spring Boot" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="MY-SQL" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
+        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="MAVEN" />
+        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
