9c6c4593e78bfe054f90e93a75e659c943cd6c44
AKSHAY MHASKE <akshaymhaske2027@gmail.com>
2024-07-07 22:49:25 +0530

    ok

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,6 @@
 import styles from './ProjectsStyle.module.css';
-import viberr from '../../assets/ems.png';
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
+import ems from '../../assets/ems.png';
+
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -11,29 +9,12 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={ems}
           link="https://github.com/akshaymhaske2027/full-stack-ems.git"
           h3="Employee Management App"
           p="Management App"
         />
-        {/* <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
-        />
-        <ProjectCard
-          src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Hipsster"
-          p="Glasses Shop"
-        /> */}
-        {/* <ProjectCard
-          src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="FitLift"
-          p="Fitness App"
-        /> */}
+      
       </div>
     </section>
   );
